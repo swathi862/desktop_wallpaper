b9888e03be3cac3fc95a45844278b39787167e9f
CaraNetzer <cara.netzer@gmail.com>
2024-06-17 22:52:32 -0400

    added windows form app

---
+++ Desktop_Wallpaper/Desktop_Wallpaper/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Desktop_Wallpaper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "C:\\Users\\caran\\workspace\\practice\\desktop_wallpaper\\Desktop_Wallpaper\\wallpaper.ps1";
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy ByPass -File \"{path}\"",
+                UseShellExecute = false
+            };
+            Process.Start(startInfo);
+
+            button1.Text = "fingers crossed!";
+        }
+    }
+}
---
+++ Desktop_Wallpaper/Desktop_Wallpaper/wallpaper.ps1
@@ -0,0 +1,29 @@
+#Retrieving image from APOD REST API https://api.nasa.gov/#apod
+#TODO: pass date and API key in as user input parameters
+$json = Invoke-WebRequest 'https://api.nasa.gov/planetary/apod?concept_tags=True&date=2024-03-05&api_key=Ec8ixequm0WkHlnG8R8ylMsaxW6tyRFip0Ws7Cir'
+#Check to see if media-type = 'image' 
+$url = ConvertFrom-Json $json | Select-Object -ExpandProperty 'hdurl'
+
+$media_type = ConvertFrom-Json $json | Select-Object -ExpandProperty 'media_type'
+# if media_type == video, return
+
+# Write-Host $url
+
+$path = 'C:\wallpapers\APOD.jpg'
+
+Invoke-WebRequest -Uri $url -OutFile $path
+
+Write-Host 'Invoke-WebRequest done- downloaded'
+Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper
+
+# Setting path to empty string to reset overwritten APOD image path
+# # Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper -Value ''
+# rundll32.exe user32.dll, UpdatePerUserSystemParameters 1,True
+
+# Start-Sleep -Seconds 5
+
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper -Value $path
+rundll32.exe user32.dll, UpdatePerUserSystemParameters  1, True
+# rundll32.exe user32.dll, UpdatePerUserSystemParameters
+
+Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper
